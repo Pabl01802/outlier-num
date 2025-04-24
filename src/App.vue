44663e6bd3194251bd3a8963ea17e11f03a5427f
Pabl01802 <58702798+Pabl01802@users.noreply.github.com>
2025-04-24 18:23:20 +0200

    fix: fixed website title

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,19 @@
 import { useI18n } from "vue-i18n"
 import "vue3-toastify/dist/index.css"
 import type { MessageSchema } from "./main"
+import { watch } from "vue"
 
-const { t } = useI18n<{ message: MessageSchema }>()
+const { t, locale } = useI18n<{ message: MessageSchema }>()
 
-document.title = t("TITLE")
+watch(
+  locale,
+  () => {
+    document.title = t("TITLE")
+  },
+  {
+    immediate: true,
+  }
+)
 </script>
 
 <template>
